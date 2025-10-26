ef36ed97db7d602660cb22c5c585e756ed63d9f7
BearzRcool <markos-tcspas@outlook.com>
2025-10-26 14:45:53 -0700

    start game

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+pygame.init()
+screen = pygame.display.set_mode((400,600))
+
+class Block():
+    pass
+
+
+while True:
+
+
+
+
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            pygame.quit()
+
+    pygame.display.update()
+    pygame.display.flip()
